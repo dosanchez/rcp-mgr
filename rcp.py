b4f79410e734fa274f3c27f7253f306106346f71
Donaldo Sanchez <donaldos@hotmail.com>
2022-07-02 19:47:41 -0400

    202272-1

--- rcp.py
+++ rcp.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField, SelectField, DecimalField
+from wtforms.validators import DataRequired, AnyOf, Length
 
+#initialization
 app=Flask(__name__)
-bootstrap = Bootstrap(app)
+app.config['SECRET_KEY']="vixxux"
+
+#forms
+class Unitmeas(FlaskForm):
+    id = IntegerField ('Reg.No.')
+    uni_un_t = SelectField('UM Type', validators=[AnyOf(values=['g','ml','unit'])])
+    uni_conv = DecimalField('Equiv. qty',validators=[DataRequired()])
+    uni_symb = StringField('name', validators=[DataRequired(),Length(max=8)])
+
 
 @app.route('/')
-def base():
-    return render_template ('base.html')
+def index():
+    return render_template ('index.html')
+
+@app.route('/templates/unitmeas.html')
+def unitmeas():
+    return render_template ('index.html')
 
 
 if __name__ == '__main__':

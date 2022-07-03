3601bad682a7faa01b386332d65b0a7c8a2019b7
Donaldo Sanchez <donaldos@hotmail.com>
2022-07-03 16:12:30 -0400

    202273-1

--- rcp.py
+++ rcp.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, SelectField, DecimalField
-from wtforms.validators import DataRequired, AnyOf, Length
+from wtforms.validators import DataRequired, AnyOf, Length, NumberRange
 
 #initialization
 app=Flask(__name__)
-app.config['SECRET_KEY']="vixxux"
+app.config['SECRET_KEY']="place a key here"
 
 #forms
 class Unitmeas(FlaskForm):
     id = IntegerField ('Reg.No.')
-    uni_un_t = SelectField('UM Type', validators=[AnyOf(values=['g','ml','unit'])])
-    uni_conv = DecimalField('Equiv. qty',validators=[DataRequired()])
-    uni_symb = StringField('name', validators=[DataRequired(),Length(max=8)])
+    qty_um = DecimalField('Qty',validators=[DataRequired(), NumberRange(min=0.001)])
+    uni_symb = StringField('Unit of measure', validators=[DataRequired(),Length(max=8)])
+    qty_base = DecimalField('Qty',validators=[DataRequired(), NumberRange(min=0.001)])
+    uni_un_t = SelectField('UM Type', validators=[AnyOf(values=['g','ml'])])
+
 
 
 @app.route('/')
@@ -21,7 +23,7 @@ def index():
 
 @app.route('/templates/unitmeas.html')
 def unitmeas():
-    return render_template ('index.html')
+    return render_template ('unitmeas.html')
 
 
 if __name__ == '__main__':

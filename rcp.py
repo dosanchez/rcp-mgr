83cbb43ffcbec9933ada74f8a080280055140d6a
Donaldo Sanchez <donaldos@hotmail.com>
2022-07-07 08:22:25 -0400

    202277-1

--- rcp.py
+++ rcp.py
@@ -24,7 +24,7 @@ class Unitmeas(FlaskForm):
     uni_symb = StringField('Unit of measure', validators=[DataRequired(),Length(max=8)], render_kw={"placeholder": "Unit of measure"})
     qty_base = DecimalField('Qty',validators=[DataRequired(), NumberRange(min=0.001)])
     uni_un_t = SelectField('UM Type', validators=[AnyOf(values=['g','ml'])], choices=['g','ml'])
-    nav = SelectField('UM Type', validators=[AnyOf(values=[-1,0,1])])
+    #nav = SelectField('UM Type', validators=[AnyOf(values=[-1,0,1])], )
 
 
 @app.route('/')
@@ -36,26 +36,29 @@ def unitmeas():
     qty_um = None
     uni_symb = None
     qty_base = None
-    uni_un_t = 'g'
-    nav = 0
+    uni_un_t = None
+    #nav = None
 
     form = Unitmeas()
     if form.validate_on_submit():
-        qty_um=form.qty_um.data
-        uni_symb=form.uni_symb.data
-        qty_base=form.qty_base.data
-        uni_un_t=form.uni_un_t.data
+        val_qty_um=form.qty_um.data
+        val_uni_symb = form.uni_symb.data
+        val_qty_base = form.qty_base.data
+        val_uni_un_t=form.uni_un_t.data
         form.qty_um.data = ''
         form.uni_symb.data = ''
         form.qty_base.data = ''
-        form.uni_un_t.data = ''
-
-        db.execute("INSERT INTO unitmeas (uni_symb, uni_conv, uni_un_t) VALUES (uni_symb, qty_base / qty_um, uni_un_t)")
-        db.commit()
+        form.uni_un_t.data = 'g'
+        #form.nav.data = 0
+        print (val_uni_symb, val_qty_base, val_qty_um)
+        db.execute("INSERT INTO unitmeas (uni_symb, uni_conv, uni_un_t) VALUES (val_uni_symb , 29 / 1, 'g')")
+        conn.commit()
         
         list = db.execute("SELECT * FROM unitmeas")
 
-    return render_template ('unitmeas.html', form=form )
+    print (list)
+
+    return render_template ('unitmeas.html', form=form, list=list )
 
 
 if __name__ == '__main__':

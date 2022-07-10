575621e65ad2e47ec420488d759baf06960536d3
Donaldo <donaldos@hotmail.com>
2022-07-10 19:58:04 -0400

    2022710-1

--- rcp.py
+++ rcp.py
@@ -1,8 +1,8 @@
 import mysql.connector
 from crypt import methods
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, SelectField, DecimalField
+from wtforms import StringField, SubmitField, IntegerField, SelectField, DecimalField, HiddenField
 from wtforms.validators import DataRequired, AnyOf, Length, NumberRange
 
 
@@ -19,7 +19,7 @@ app.config['SECRET_KEY']="place a key here"
 
 #forms
 class Unitmeas(FlaskForm):
-    id = IntegerField ('Reg.No.')
+    id = HiddenField()
     qty_um = DecimalField('Qty',validators=[DataRequired(), NumberRange(min=0.001)], 
         render_kw={"placeholder": "qty"}, default = 1)
     uni_symb = StringField('Unit of measurement', validators=[DataRequired(),Length(max=8)], 
@@ -28,7 +28,6 @@ class Unitmeas(FlaskForm):
         render_kw={"placeholder": "qty"}, default = 1)
     uni_un_t = SelectField('UM Type', validators=[AnyOf(values=['g','ml'])], 
         choices=['g','ml'])
-    nav = IntegerField('UM Type',default = 0 )
 
 #view funtions
 @app.route('/')
@@ -69,29 +68,45 @@ def unitmeas():
             return redirect(url_for('unitmeas'))# clears POST data 
 
 
-    #visualize registered units of measurement and moves form to nav target
-    
-    if form.nav.data == 0:
-        sql = "SELECT MAX(id) FROM unitmeas"
-        db.execute(sql)
-        max_id =db.fetchone().value('MAX(id)')
-        sql = "SELECT * FROM unitmeas WHERE id = max_id"
-        db.execute(sql)
-        records = db.fetchone()
-        form.qty_um.data = 1
-        form.uni_symb.data = records.get('uni_symb')
-        form.qty_base.data = records.get('uni_conv')
-        form.uni_un_t.data = records.get('uni_un_t')
-        form.nav.data = max_id
-    
+    #visualize registered U.M and moves form to nav target
     sql = "Select * from unitmeas"
     db.execute(sql)
     records = db.fetchall()
     column_names = db.column_names
 
+    regd_id =[row.get('id') for row in records] #making a list of ids
+    num_regd_id = len(regd_id) #calc id list lenght
+    
+  
+    print(session['id'])
+    if form.id.data == None:
+         form.id.data = regd_id[num_regd_id -1]
+    elif request.form.get('nav') == "first":
+        form.id.data = regd_id[0] 
+    elif request.form.get('nav') == "last":
+        form.id.data = regd_id[num_regd_id-1]
+    elif request.form.get('nav') == "back" and regd_id.index(form.id.data) > 0:
+        form.id.data = regd_id[regd_id.index(form.id.data)-1]
+    elif request.form.get('nav') == "back" and regd_id.index(form.id.data) == 0:
+        form.id.data = regd_id[0]
+    elif request.form.get('nav') == "next" and regd_id.index(form.id.data) < (len(regd_id) - 1):
+        form.id.data = regd_id[regd_id.index(form.id.data)+1]
+    elif request.form.get('nav') == "next" and regd_id.index(form.id.data) == (len(regd_id) - 1):
+        form.id.data = regd_id[num_regd_id-1]
+    else:
+        form.id.data =regd_id[num_regd_id-1]
+
+    #visualize the target record
+    sql = "SELECT * FROM unitmeas WHERE id = %s"
+    db.execute(sql, (form.id.data,))
+    tgt_record = db.fetchone()
+    form.qty_um.data = 1
+    form.uni_symb.data = tgt_record.get('uni_symb')
+    form.qty_base.data = tgt_record.get('uni_conv')
+    form.uni_un_t.data = tgt_record.get('uni_un_t')
 
     return render_template ('unitmeas.html', form=form, records=records,
-                                                    column_names=column_names)
+                            column_names=column_names)
 
 
 if __name__ == '__main__':

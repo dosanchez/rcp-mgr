6444d8425389293a6e2312ec9aceff31c6df9ced
Donaldo Sanchez <donaldos@hotmail.com>
2022-07-24 18:23:07 -0400

    2022724-1

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ each form can navigate through records while listing them all at the bottom.
 
 data format to pass to datahandler class (db, {table1name:[{record1field1name:record1field1value},{record1field2name:record1field2value},{record2field1name:record2field1value},{record2field2name:record2field2value}...],table2name:[{record1field1name:record1field1value},{record1field2name:record1field2value},...]})
 
+All wtf Classes are in forms.py
+nav.py takes care of form navigation and returns set of registered records
---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, DecimalField, HiddenField
+from wtforms.validators import DataRequired, AnyOf, Length, NumberRange
+
+class Unitmeas(FlaskForm):
+    """wtform for Units of measure"""
+    id = HiddenField()
+    qty_um = DecimalField('Qty',validators=[DataRequired(), NumberRange(min=0.001)], 
+        render_kw={"placeholder": "qty"}, default = 1)
+    uni_symb = StringField('Unit of measurement', validators=[DataRequired(),Length(max=8)], 
+        render_kw={"placeholder": "Unit of measurement"})
+    qty_base = DecimalField('Qty',validators=[DataRequired(), NumberRange(min=0.001)], 
+        render_kw={"placeholder": "qty"}, default = 1)
+    uni_un_t = SelectField('UM Type', validators=[AnyOf(values=['g','ml'])], 
+        choices=['g','ml'])

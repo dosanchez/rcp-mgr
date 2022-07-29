941637129a04cac30b88424dc62cc52083be32d8
Donaldo Sanchez <donaldos@hotmail.com>
2022-07-29 14:45:08 -0400

    2022729-1

--- forms.py
+++ forms.py
@@ -1,18 +1,34 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, DecimalField, HiddenField, BooleanField
+from wtforms import StringField, SelectField, DecimalField, HiddenField
+from wtforms import BooleanField, Form, FormField, FieldList
 from wtforms.validators import DataRequired, Length, NumberRange
 
+
+class Recet_de(Form):
+    """wtform for recipe form details"""
+    id = HiddenField()
+    rcd_enca = HiddenField()
+    rcd_ing = SelectField('Ingredient', validators=[DataRequired()])
+    rcd_qty = DecimalField('Ingredient density',validators=[DataRequired()],
+                            default = 1)
+    rcd_ing = SelectField('Ingredient UM', validators=[DataRequired()])
+    rcd_yiel = DecimalField('Ingredient yield', validators=[DataRequired()],
+                            default = 0.98)
+    
 class Unitmeas(FlaskForm):
     """wtform for Units of measure"""
     id = HiddenField()
-    qty_um = DecimalField('Qty',validators=[DataRequired(), NumberRange(min=0.001)], 
-        render_kw={"placeholder": "qty"}, default = 1)
-    uni_symb = StringField('Unit of measurement', validators=[DataRequired(),Length(max=8)], 
-        render_kw={"placeholder": "Unit of measurement"})
-    qty_base = DecimalField('Qty',validators=[DataRequired(), NumberRange(min=0.001)], 
-        render_kw={"placeholder": "qty"}, default = 1)
+    qty_um = DecimalField('Qty',validators=[DataRequired(), 
+                            NumberRange(min=0.001)], 
+                            render_kw={"placeholder": "qty"}, default = 1)
+    uni_symb = StringField('Unit of measurement', validators=[DataRequired(),
+                            Length(max=8)], 
+                            render_kw={"placeholder": "Unit of measurement"})
+    qty_base = DecimalField('Qty',validators=[DataRequired(), 
+                            NumberRange(min=0.001)], 
+                            render_kw={"placeholder": "qty"}, default = 1)
     uni_un_t = SelectField('UM Type', validators=[DataRequired()], 
-        choices=['g','ml'], default = 'g')
+                            choices=['g','ml'], default = 'g')
     uni_ebld = BooleanField('Enabled', default = True, false_values=('',))
 
 class Almacen(FlaskForm):
@@ -35,3 +51,23 @@ class Ingredient(FlaskForm):
         choices=['g/ml','g/unit'], default ='g/ml')
     ing_rece = BooleanField('Recipe', default = False, false_values=('',), render_kw = {'disabled':''})
     ing_ebld = BooleanField('Enabled', default = True, false_values=('',))
+
+class Recet_en(FlaskForm):
+    """wtform for recipe form header"""
+    id = HiddenField()
+    rct_name = StringField('Recipe/Plate', validators=[DataRequired(),Length(max=16)], 
+        render_kw={"placeholder": "e.g. French Fries Side"})
+    rct_cost = DecimalField('Actual Cost',validators=[DataRequired()], 
+        render_kw = {'disabled':''}, default = 0)
+    rct_cosc = DecimalField('Standard Cost',validators=[DataRequired()], 
+        render_kw = {'disabled':''}, default = 0)
+    rct_dens = DecimalField('Recipe/Plate density',validators=[DataRequired()], 
+        render_kw={"placeholder": "density"}, default = 1)
+    rct_denu = SelectField('Recipe/Plate density UM', validators=[DataRequired()], 
+        choices=['g/ml','g/unit'], default ='g/unit')
+    rct_yiel = DecimalField('Standard Cost',validators=[DataRequired()], 
+        render_kw = {'disabled':''}, default = 1)
+    rct_unit = SelectField('Common UM', validators=[DataRequired()], 
+        default = 'g')
+    rct_ebld = BooleanField('Enabled', default = True, false_values=('',))
+    rct_subf = FormField(Recet_de)
--- rcp.py
+++ rcp.py
@@ -186,14 +186,77 @@ def ingredient():
     column_names =['', 'Ingredient', 'Common UM', 'Density', 'Densi UM',
                      'Recipe','Enabled']
 
-
-    print(records)
-    print(form.ing_unit.choices)
-    print(form.ing_unit.data)
-    print(ing_unit_choices)
-
     return render_template ('ingredient.html', form = form, records = records,
                             column_names = column_names, ing_unit_choices = ing_unit_choices)
 
+@app.route('/templates/recipe.html', methods=['GET','POST'])
+def recipe():
+    form = Recet_en()
+    sqltable = 'recet_en'
+    sqltable1 = 'recet_de'
+
+    #Queries for Selectfields active choices
+    sql = """SELECT id, uni_symb 
+            FROM unitmeas
+            WHERE uni_ebld = True"""
+    
+    db.execute(sql)
+    form.ing_unit.choices = sorted([(d['id'],
+     d['uni_symb']) for d in list(db.fetchall())], key = lambda fld: fld[1])
+
+    #Queries for all possible Selectfields choices
+    sql = """SELECT id, uni_symb 
+            FROM unitmeas"""
+    db.execute(sql)
+    ing_unit_choices = [(d['id'], d['uni_symb']) for d in list(db.fetchall())]
+
+        
+    nav_button =  request.form.get('nav') #saves form navigation request
+    try:
+        nav_button = int(nav_button)
+    except:
+        pass
+    
+
+    if form.validate_on_submit():
+        #Selectfield values
+
+        record = dth.from_dict2sql(conn, {
+                                        sqltable:[{
+                                            'id':int(form.id.data),
+                                            'ing_name':form.ing_name.data,
+                                            'ing_unit':form.ing_unit.data,
+                                            'ing_dens':form.ing_dens.data,
+                                            'ing_denu':form.ing_denu.data,
+                                            'ing_rece':form.ing_rece.data,
+                                            'ing_ebld':form.ing_ebld.data  
+                                                    }]
+                                        })
+
+        if nav_button == "submit": #not a nav post
+            #creates instance to chk if record exist
+            existe = dth.from_dict2sql(conn, {
+                                        sqltable:[{
+                                            'id':int(form.id.data)
+                                                    }]
+                                        }
+            ) 
+            if  existe.chk_sgl_fld():   #chk if record exists   
+                #update existing record
+                record.update()
+                return redirect(url_for('ingredient'))# clears POST data
+
+            else:
+                #adds new record
+                record.add_new()
+                return redirect(url_for('ingredient'))# clears POST data 
+
+
+    records = navigate_to(nav_button, db, form, sqltable)
+    column_names =['', 'Ingredient', 'Common UM', 'Density', 'Densi UM',
+                     'Recipe','Enabled']
+
+    return render_template ('ingredient.html', form = form, records = records,
+                            column_names = column_names, ing_unit_choices = ing_unit_choices)
 if __name__ == '__main__':
     app.run(debug=True)
